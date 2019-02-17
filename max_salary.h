1e74b40635bd2663b7ef12208fcb5500073cd632
Boyuan Jia <bjia18@src-code.simons-rock.com>
2019-02-17 11:17:44 -0500

    done

--- max_salary.h
+++ max_salary.h
@@ -4,9 +4,18 @@
 #include <stdio.h>
 #include <time.h>
 #include <stdlib.h>
+#include <string.h>
+#include <limits.h>
 
 #define SWAP(a,b) {int temp; temp=a; a=b; b=temp;}
+#define LEN ((CHAR_BIT * sizeof(int) - 1) / 3 + 2)+1
 
+extern double h_max;
+double naive(int *a,int n);
+void stress_test(int n, int m);
+void copy_array(int *a,int *c,int n);
+int is_better(const void *a, const void *b);
+double combine_max(int *a, int n);
 void print_array(int *arr, int size);
 void heap_permutation(int a[], int size, int n) ;
 
--- max_salary_naive.c
+++ max_salary_naive.c
@@ -8,6 +8,42 @@ void print_array(int *arr, int size){
   }
   printf("\n");
 }
+double combine_max(int *c, int n){
+  char str[LEN*n];
+  sprintf(str, "%d", c[0]);
+  for (int i=1; i<n; i++){
+    sprintf(str+strlen(str), "%d", c[i]);
+  }
+  //printf("STR: %s\n",str);
+  double max=atof(str);
+  return max;
+}
+int is_better(const void *a,const void *b){
+  const int *ia=(const int *)a;
+  const int *ib=(const int *)b;
+  char str1[LEN],str2[LEN],temp[LEN];
+  sprintf(str1, "%d", *ia);
+  sprintf(temp, "%d", *ib);
+  strcpy(str2,temp);
+  strcat(str2,str1);//str2 in front
+  strcat(str1,temp);//str1 in front
+  //printf("STR: %s %s\n",str1,str2);
+  return (atof(str2)>atof(str1))?(1):(0);//str2 vs str1
+  //printf("atoi: %d\n",a[0]);
+}
+void copy_array(int *a,int *c,int n){
+  for (int i=0; i<n; i++){
+    c[i]=a[i];
+  }
+}
+double h_max=0;
+double naive(int *a,int n){
+        int c[n];
+        copy_array(a,c,n);
+        qsort(c, n, sizeof(int), is_better);
+        double max=combine_max(c,n);
+        return max;
+}
 
 // Generating permutation using Heap Algorithm 
 void heap_permutation(int a[], int size, int n) 
@@ -16,7 +52,9 @@ void heap_permutation(int a[], int size, int n)
     if (size == 1) 
     { 
         //TO DO - instead of printing, insert your code for evaluating the next candidate array
-        print_array(a, n); 
+        double temp=combine_max(a,n);
+        if (temp>h_max)
+          h_max=temp;
         return; 
     } 
   
@@ -27,18 +65,12 @@ void heap_permutation(int a[], int size, int n)
         // if size is odd, swap first and last element 
         if (size%2==1) {
             SWAP(a[0], a[size-1]); 
-		}
+		    }
         // If size is even, swap ith and last element 
         else{
             SWAP(a[i], a[size-1]);
-		}			
+		    }			
     } 
 } 
   
-// Code to test permutations
-int main() { 
-    int a[] = {1, 2, 3, 4}; 
-    int n = sizeof a/sizeof a[0]; 
-    heap_permutation(a, n, n); 
-    return 0; 
-} 
+
